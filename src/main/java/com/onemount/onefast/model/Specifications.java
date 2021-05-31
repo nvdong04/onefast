7a827322ead2b70a2bbce1fbdd44902da2653b46
Nguyễn Thị Ngọc (1MG-KCN-MB) <ngocnt56@vingroup.net>
2021-05-31 13:33:41 +0700

    edit1

--- src/main/java/com/onemount/onefast/model/Specifications.java
+++ src/main/java/com/onemount/onefast/model/Specifications.java
@@ -1,5 +1,6 @@
 package com.onemount.onefast.model;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -12,4 +13,22 @@ public class Specifications {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(name = "battery")
+    private Long battery;
+
+    @Column(name = "weight")
+    private Long weight;
+
+    @Column(name = "size")
+    private Long size;
+
+    @Column(name = "exterior")
+    private String exterior;
+
+    @Column(name = "furniture")
+    private String furniture;
+
+    @Column(name = "smart_feature")
+    private String SmartFeature;
 }
