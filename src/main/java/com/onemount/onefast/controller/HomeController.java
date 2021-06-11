87e9bba102b2c0dd4ed97265b008cda3f79ab7ba
nvdong04 <39474740+nvdong04@users.noreply.github.com>
2021-06-11 22:24:30 +0700

    Update HomeController.java

--- src/main/java/com/onemount/onefast/controller/HomeController.java
+++ src/main/java/com/onemount/onefast/controller/HomeController.java
@@ -3,6 +3,7 @@ package com.onemount.onefast.controller;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping(value = "/")
 public class HomeController {
     public String root() {
         return "Welcome!";
