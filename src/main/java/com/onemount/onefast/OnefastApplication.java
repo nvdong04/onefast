8e2a3b0674254a823731c7acfa1f1018b8d89e84
Vandonger <vandong1x99@gmail.com>
2021-06-12 22:52:45 +0700

    fixed error create bean passencoder

--- src/main/java/com/onemount/onefast/OnefastApplication.java
+++ src/main/java/com/onemount/onefast/OnefastApplication.java
@@ -5,7 +5,10 @@ import com.onemount.onefast.repository.UserRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 @EnableJpaRepositories(basePackageClasses = UserRepository.class)
@@ -13,6 +16,11 @@ public class OnefastApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(OnefastApplication.class, args);
+	}
+
+	@Bean
+	public BCryptPasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
 	}
 
 }
--- src/main/java/com/onemount/onefast/service/UserServiceImpl.java
+++ src/main/java/com/onemount/onefast/service/UserServiceImpl.java
@@ -20,7 +20,6 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
-import com.onemount.onefast.service.*;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
@@ -29,7 +28,6 @@ public class UserServiceImpl implements UserService{
 
     @Autowired
     private UserRepository userRepository;
-
     @Autowired
     private BCryptPasswordEncoder passwordEncoder;
 
